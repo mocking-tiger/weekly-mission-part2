2824f0179dedd6a16beee4048229c5418aace017
Park JunYong <dyden119@gmail.com>
2023-12-30 00:26:41 +0900

    feat: 메인(사용자프로필&폴더이름) 완성

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -14,12 +14,12 @@ function Footer() {
     <footer>
       <div>
         <div>
-          <div class="foot-left">©codeit - 2023</div>
-          <div class="foot-center">
+          <div className="foot-left">©codeit - 2023</div>
+          <div className="foot-center">
             <a href="./privacy/privacy.html">Privacy Policy</a>
             <a href="./faq/faq.html">FAQ</a>
           </div>
-          <div class="foot-right">
+          <div className="foot-right">
             <a rel="noreferrer" href="http://faceboom.com" target={"_blank"}><img src={facebook} alt="facebook" /></a>
             <a rel="noreferrer" href="https://twitter.com/?lang=ko" target="_blank"><img src={twitter} alt="twit" /></a>
             <a rel="noreferrer" href="https://www.youtube.com/" target="_blank"><img src={youtube} alt="youTube" /></a>
