09ad6fb860bbb1613cb28258907f66068e3b9b1a
Park JunYong <dyden119@gmail.com>
2023-12-30 23:52:06 +0900

    feat: api의 createdAt 데이터와 현재날짜 계산해서 출력 구현

--- src/Section/Section.js
+++ src/Section/Section.js
@@ -4,8 +4,8 @@ import Card from "./Card/Card";
 import { useEffect, useState } from "react";
 
 function Section() {
-  function today() {
-    let today = new Date();
+  let today = new Date();
+  function todayIs() {
     return today.toLocaleDateString();
   }
   let [cardInfo, setCardInfo] = useState();
@@ -34,6 +34,45 @@ function Section() {
   if (!cardInfo) {
     return null;
   }
+  const timeDiffs = [];
+  const dateCalculator = async () => {
+    for (let i = 0; i < 9; i++) {
+      let linkedDay = new Date(cardInfo[i].createdAt);
+      let timeDiff = today - linkedDay;
+      timeDiffs.push(Math.floor(timeDiff / 1000));
+      console.log(timeDiffs[i]);
+    }
+    await Promise.allSettled(timeDiffs);
+  };
+
+  dateCalculator();
+
+  function timeDiffChecker(num) {
+    const seconds = 1;
+    const minute = seconds * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const mon = day * 30;
+    if (num < minute * 2) {
+      return `1 minute ago`;
+    } else if (num < minute * 59) {
+      return `${Math.trunc(num / 60)} minutes ago`;
+    } else if (num > minute * 60 && num < day) {
+      return `${Math.trunc(num / 60 / 60)} hours ago`;
+    } else if (num > day && num < day * 2) {
+      return `1 day ago`;
+    } else if (num < day * 30) {
+      return `${Math.trunc(num / 60 / 60 / 24)} days ago`;
+    } else if (num >= day * 30 && num < day * 60) {
+      return `1 month ago`;
+    } else if (num < mon * 12) {
+      return `${Math.trunc(num / 60 / 60 / 24 / 30)} months ago`;
+    } else if (num >= mon * 12 && num < mon * 24) {
+      return `1 year ago`;
+    } else if (num >= mon * 24) {
+      return `${Math.trunc(num / 60 / 60 / 24 / 30 / 12)} years ago`;
+    }
+  }
   console.log(cardInfo);
   return (
     <section>
@@ -45,9 +84,9 @@ function Section() {
           <Card
             style={cardInfo[0].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[0].imageSource}
-            createdAt={cardInfo[0].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[0])}
             description={cardInfo[0].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
             className="card"
           />
         </a>
@@ -55,72 +94,72 @@ function Section() {
           <Card
             style={cardInfo[1].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[1].imageSource}
-            createdAt={cardInfo[1].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[1])}
             description={cardInfo[1].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[2].url} target={"_blank"}>
           <Card
             style={cardInfo[2].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[2].imageSource}
-            createdAt={cardInfo[2].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[2])}
             description={cardInfo[2].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[3].url} target={"_blank"}>
           <Card
             style={cardInfo[3].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[3].imageSource}
-            createdAt={cardInfo[3].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[3])}
             description={cardInfo[3].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[4].url} target={"_blank"}>
           <Card
             style={cardInfo[4].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[4].imageSource}
-            createdAt={cardInfo[4].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[4])}
             description={cardInfo[4].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[5].url} target={"_blank"}>
           <Card
             style={cardInfo[5].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[5].imageSource}
-            createdAt={cardInfo[5].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[5])}
             description={cardInfo[5].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[6].url} target={"_blank"}>
           <Card
             style={cardInfo[6].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[6].imageSource}
-            createdAt={cardInfo[6].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[6])}
             description={cardInfo[6].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[7].url} target={"_blank"}>
           <Card
             style={cardInfo[7].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[7].imageSource}
-            createdAt={cardInfo[7].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[7])}
             description={cardInfo[7].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
         <a rel="noreferrer" href={cardInfo[8].url} target={"_blank"}>
           <Card
             style={cardInfo[8].imageSource !== undefined ? style : logoStyle}
             image={cardInfo[8].imageSource}
-            createdAt={cardInfo[8].createdAt}
+            createdAt={timeDiffChecker(timeDiffs[8])}
             description={cardInfo[8].description}
-            uploadDate={today()}
+            uploadDate={todayIs()}
           />
         </a>
       </div>
