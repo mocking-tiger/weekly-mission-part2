fa00e8d7db36be7c084adde2d828ef07649af7b0
Park JunYong <dyden119@gmail.com>
2023-12-31 01:11:47 +0900

    feat: 카드컴포넌트 태블릿에서2열, 모바일에서 1열

--- src/Section/Search/Search.js
+++ src/Section/Search/Search.js
@@ -4,12 +4,14 @@ import magnifier from "../../assets/Search.svg";
 function Search() {
   return (
     <div className="search-box">
-      <input
-        type="text"
-        placeholder="링크를 검색해 보세요."
-        className="search-bar"
-      />
-      <img src={magnifier} alt="magnifier" />
+      <label>
+        <input
+          type="text"
+          placeholder="링크를 검색해 보세요."
+          className="search-bar"
+        />
+        <img src={magnifier} alt="magnifier" />
+      </label>
     </div>
   );
 }
