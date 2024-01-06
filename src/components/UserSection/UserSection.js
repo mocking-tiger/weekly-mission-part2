189c2ba738d55050a8b0b2ccad689da2ca746dfe
Park JunYong <dyden119@gmail.com>
2024-01-07 02:13:10 +0900

    feat: 버튼으로 즐겨찾기 필터링

--- src/components/UserSection/UserSection.js
+++ src/components/UserSection/UserSection.js
@@ -9,71 +9,11 @@ import pen from "../../assets/pen.svg";
 import share from "../../assets/share.svg";
 import remove from "../../assets/delete.svg";
 
-function CreateButton() {
-  const [buttonInfo, setButtonInfo] = useState();
-  const [selectedButton, setSelectedButton] = useState("전체");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`${CODEIT_API}/users/1/folders`);
-      const data = await response.json();
-      setButtonInfo(data["data"]);
-    };
-    fetchData();
-  }, []);
-
-  if (!buttonInfo) {
-    return null;
-  }
-
-  const handleButtonClick = (name) => {
-    setSelectedButton(name);
-  };
-
-  return (
-    <nav>
-      <div className="button-button-area">
-        <button
-          onClick={() => handleButtonClick("전체")}
-          className={`${selectedButton === "전체" ? "selected" : ""}`}
-        >
-          전체
-        </button>
-        {buttonInfo.map((item) => (
-          <button
-            onClick={() => handleButtonClick(item.name)}
-            key={item.id}
-            className={item.name === selectedButton ? "selected" : ""}
-          >
-            {item.name}
-          </button>
-        ))}
-      </div>
-      <div className="button-text-area">
-        <p>{selectedButton}</p>
-        <div
-          className={`tool-box ${selectedButton === "전체" ? "hidden" : ""}`}
-        >
-          <a href="#!">
-            <img src={share} alt="share" />
-            공유
-          </a>
-          <a href="#!">
-            <img src={pen} alt="pen" />
-            이름 변경
-          </a>
-          <a href="#!">
-            <img src={remove} alt="delete" />
-            삭제
-          </a>
-        </div>
-      </div>
-    </nav>
-  );
-}
-
 function UserSection() {
-  const [cardInfo, setCardInfo] = useState();
+  const [cardInfo, setCardInfo] = useState([]);
+  const [buttonInfo, setButtonInfo] = useState([]);
+  const [selectedButton, setSelectedButton] = useState("전체");
+  const [filterdData, setFilteredData] = useState([]);
   const style = {};
   const logoStyle = {
     opacity: "0.2",
@@ -83,16 +23,35 @@ function UserSection() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`${CODEIT_API}/users/1/links`);
-      const data = await response.json();
-      setCardInfo(data["data"]);
+      const responseFolders = await fetch(`${CODEIT_API}/users/1/folders`);
+      const folderData = await responseFolders.json();
+
+      const responseLinks = await fetch(`${CODEIT_API}/users/1/links`);
+      const linkData = await responseLinks.json();
+
+      setButtonInfo(folderData["data"]);
+      setCardInfo(linkData["data"]);
+      setFilteredData(linkData["data"]);
     };
     fetchData();
   }, []);
 
-  if (!cardInfo) {
+  if (!buttonInfo && !cardInfo) {
     return null;
   }
+
+  const handleButtonClick = (name) => {
+    setSelectedButton(name);
+  };
+
+  const handleFilter = (category) => {
+    if (category === "전체") {
+      setFilteredData(cardInfo);
+    } else {
+      const filterd = cardInfo.filter((item) => item["folder_id"] === category);
+      setFilteredData(filterd);
+    }
+  };
 
   const timeDiffs = cardInfo.map((item) => {
     const today = new Date();
@@ -101,29 +60,74 @@ function UserSection() {
   });
 
   return (
-    <section>
-      <CreateButton />
-      <div className="div-card">
-        {cardInfo.map((cardData, index) => (
-          <a
-            rel="noreferrer"
-            href={cardData.url}
-            target={"_blank"}
-            key={cardData.id}
+    <>
+      <nav>
+        <div className="button-button-area">
+          <button
+            onClick={() => {
+              handleButtonClick("전체");
+              handleFilter("전체");
+            }}
+            className={`${selectedButton === "전체" ? "selected" : ""}`}
           >
-            <Card
-              style={cardData.image_source !== null ? style : logoStyle}
-              image={
-                cardData.image_source !== null ? cardData.image_source : logo
-              }
-              createdAt={timeDiffChecker(timeDiffs[index])}
-              description={cardData.description}
-              uploadDate={todayIs()}
-            />
-          </a>
-        ))}
-      </div>
-    </section>
+            전체
+          </button>
+          {buttonInfo.map((item) => (
+            <button
+              onClick={() => {
+                handleButtonClick(item.name);
+                handleFilter(item.id);
+              }}
+              key={item.id}
+              className={item.name === selectedButton ? "selected" : ""}
+            >
+              {item.name}
+            </button>
+          ))}
+        </div>
+        <div className="button-text-area">
+          <p>{selectedButton}</p>
+          <div
+            className={`tool-box ${selectedButton === "전체" ? "hidden" : ""}`}
+          >
+            <a href="#!">
+              <img src={share} alt="share" />
+              공유
+            </a>
+            <a href="#!">
+              <img src={pen} alt="pen" />
+              이름 변경
+            </a>
+            <a href="#!">
+              <img src={remove} alt="delete" />
+              삭제
+            </a>
+          </div>
+        </div>
+      </nav>
+      <section>
+        <div className="div-card">
+          {filterdData.map((cardData, index) => (
+            <a
+              rel="noreferrer"
+              href={cardData.url}
+              target={"_blank"}
+              key={cardData.id}
+            >
+              <Card
+                style={cardData.image_source !== null ? style : logoStyle}
+                image={
+                  cardData.image_source !== null ? cardData.image_source : logo
+                }
+                createdAt={timeDiffChecker(timeDiffs[index])}
+                description={cardData.description}
+                uploadDate={todayIs()}
+              />
+            </a>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
 
